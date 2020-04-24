3129470e6abf669ed9c321803c5c77a29eaf927f
Allen Yuan <alleyuan@amazon.com>
2020-04-24 01:04:44 -0700

    day4

---
+++ graph/MaxNetworkRank.java
@@ -0,0 +1,53 @@
+package graph;
+
+/*
+
+Microsoft 2019: Max Network Rank
+
+Question:
+N cities 1, 2, ... , N. M bidirectional roads between them. For each pair of cities connected by a road,
+define their network rank as the total number of roads connected to either of the two cities.
+Find the max network rank.
+
+Signature:
+int maxNetworkRank(int[] A, int[] B, int N) where N = # of cities and A, B are arrays of size M where
+A[i], B[i] = cities at the ends of road i.
+
+Solution:
+Form a graph where the cities are the vertices and roads are the edges. Calculate the indegree function and
+adjacency matrix, then find the max value of indegree(A) + indegree(B) - #roads-between-a-and-b.
+
+*/
+
+import java.util.*;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class MaxNetworkRank {
+  public int maxNetworkRank(int[] A, int[] B, int N) {
+    int M = A.length;
+    // indegree. we use N+1 on these arrays to make indexing easier
+    int[] indegree = new int[N+1];
+    // adjacency matrix. adj[i][j] = # of edges between i and j
+    int[][] adj = new int[N+1][N+1];
+    // calculate indegrees and adjacency matrix and result
+    int max = 0;
+    for(int i = 0; i < M; i++) {
+      indegree[A[i]]++;
+      indegree[B[i]]++;
+      adj[A[i]][B[i]]++;
+      adj[B[i]][A[i]]++;
+      int networkRank = indegree[A[i]] + indegree[B[i]] - adj[A[i]][B[i]] + 1;
+      max = networkRank > max ? networkRank : max;
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    int[] A = {1, 2, 3, 3};
+    int[] B = {2, 3, 1, 4};
+    int N = 4;
+    MaxNetworkRank solution = new MaxNetworkRank();
+    System.out.println(solution.maxNetworkRank(A, B, N));
+  }
+}
